6cf2ca3210770da6a0f653a166dec5ed1c048c41
Rajkumar ANGARE <rajangare@gmail.com>
2023-06-11 18:06:14 +0530

    Initial commit
    Signed-off-by: Rajkumar ANGARE <rajangare@gmail.com>

---
+++ src/main/java/com/poc/auth/AuthServicePocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServicePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServicePocApplication.class, args);
+	}
+
+}
